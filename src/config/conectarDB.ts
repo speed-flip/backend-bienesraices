7cbc6002814a6f356f769ab7b0a804e9909138dc
Victor Torres <vtorresquintanilla0206@gmail.com>
2023-11-25 11:16:57 -0600

    connect to mongo database

---
+++ src/config/conectarDB.ts
@@ -0,0 +1,16 @@
+import { connect } from 'mongoose';
+import config from '../environment';
+
+async function connectDB() {
+  try {
+    const db = await connect(config.MONGO_URI);
+
+    const url = `${db.connection.host}:${db.connection.port}`;
+    console.log(`Mongo connected to ${url}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import connectDB from './config/conectarDB';
 
-app.use('/', (req, res) => res.send('Thanks for using this boilerplate :)'));
+connectDB();
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
