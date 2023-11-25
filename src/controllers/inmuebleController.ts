0d10b83743088968cc767f9ca1eb65bb7cf5a6c2
Victor Torres <vtorresquintanilla0206@gmail.com>
2023-11-25 15:35:42 -0600

    create: inmueble router post

--- src/controllers/inmuebleController.ts
+++ src/controllers/inmuebleController.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Response, Request } from 'express';
+
 import Inmueble from '../models/Inmueble';
+import { MulterFile } from '../lib/multer';
 
 export async function getInmuebles(req: Request, res: Response) {
   const inmuebles = await Inmueble.find();
@@ -7,6 +10,57 @@ export async function getInmuebles(req: Request, res: Response) {
     msg: 'Consulta exitosa',
     total_registros: inmuebles.length,
     inmuebles,
+    errorResponse: {
+      error: false,
+      code: 200,
+      msg: '',
+      details: '',
+    },
+  });
+}
+
+export async function crearInmueble(req: Request, res: Response) {
+  const files = req.files as MulterFile[];
+
+  if (!files || files.length == 0) {
+    res.status(404).json({
+      msg: 'Faltan las imagenes',
+    });
+    return;
+  }
+
+  const imagenes: string[] = files.map((file: MulterFile) => `/public/img/${file.filename}`);
+  const nuevoInmueble = { ...req.body, imagenes };
+
+  try {
+    const inmueble = new Inmueble(nuevoInmueble);
+    const inmuebleSaved = await inmueble.save();
+    res.status(200).json({
+      inmueble: inmuebleSaved,
+      msg: 'Inmueble creado correctamente',
+      errorResponse: {
+        error: false,
+        code: 200,
+        msg: '',
+        details: '',
+      },
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      errorResponse: {
+        error: true,
+        code: 400,
+        msg: 'Se produjo un error',
+        details: error.message,
+      }
+    });
+  }
+
+
+  res.status(200).json({
+    msg: 'Creando inmueble',
+    nuevoInmueble,
     errorResponse: {
       error: false,
       code: 200,
--- src/routes/inmuebleRoutes.ts
+++ src/routes/inmuebleRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getInmuebles } from '../controllers/inmuebleController';
+import upload from '../lib/multer';
+
+import { crearInmueble, getInmuebles } from '../controllers/inmuebleController';
+import checkIsAdmin from '../middlewares/checkIsAdmin';
 
 const router = Router();
 
 router.get('/', getInmuebles);
+router.post('/', checkIsAdmin, upload.array('imagenes', 3), crearInmueble);
 
 export default router;
