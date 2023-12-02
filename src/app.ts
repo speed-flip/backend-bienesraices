44570ec0396c0876b4b8b9a88c5c18178758a895
Victor Torres <vtorresquintanilla0206@gmail.com>
2023-12-02 12:04:04 -0600

    add: static public folder to app

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import express, { Application } from 'express';
 import cors, { CorsOptions } from 'cors';
 
-const app: Application = express()
+const app: Application = express();
+
+// middlewares
+app.use(express.static('public'));
 
 const whitelist = [process.env.FRONTEND_URL];
 
 const corsOpt: CorsOptions = {
   origin: (origin, calback) => {
-    if(whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1) {
       calback(null, true);
-    }else{
+    } else {
       calback(new Error('Not allowed by cors'));
     }
   }
-}
+};
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOpt));
 
-export default app
+export default app;
