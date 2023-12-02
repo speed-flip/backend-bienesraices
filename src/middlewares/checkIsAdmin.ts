c5dc35df8b36a81e2bff04571e6ca32a54ba77f3
Victor Torres <vtorresquintanilla0206@gmail.com>
2023-12-02 11:46:41 -0600

    update: checkIsAdmin middleware

--- src/middlewares/checkIsAdmin.ts
+++ src/middlewares/checkIsAdmin.ts
@@ -1,61 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-
-import config from '../environment';
-import { User } from '../interfaces/index';
-import Usuario from '../models/Usuario';
-
-interface IDecoded {
-  id: string,
-  iat: number,
-  exp: number,
-}
 
 async function checkIsAdmin(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization?.split(' ')[1];
-
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, config.JWT_SECRET) as IDecoded;
-      req.user = await Usuario.findById(decoded.id) as User;
-
-      if (decoded.id == '656230189a850b4886820cd2') {
-        return next();
-      } else {
-        res.status(401).json({
-          errorResponse: {
-            error: true,
-            code: 401,
-            msg: 'Acción no valida',
-            details: 'No tiene permisos para ejecutar esta acción',
-          },
-        });
-      }
-    } catch (error: any) {
-      console.log(error);
-      res.status(500).json({
-        errorResponse: {
-          error: true,
-          code: 500,
-          msg: 'Se produjo un error',
-          details: error.message,
-        },
-      });
-    }
-  }
 
   if (!token) {
     res.status(404).json({
       errorResponse: {
         error: true,
         code: 404,
-        msg: 'Identificador de sesión no encontrado',
+        msg: 'Identificador de sesión de administrador no encontrado',
         details: '',
       },
     });
     return;
   }
+
+  if (req.user?.email == 'victor@torres.com') {
+    return next();
+  } else {
+    res.status(401).json({
+      errorResponse: {
+        error: true,
+        code: 401,
+        msg: 'Acción no valida',
+        details: 'No tiene permisos para ejecutar esta acción',
+      },
+    });
+  }
+
 }
 
 export default checkIsAdmin;
--- src/routes/inmuebleRoutes.ts
+++ src/routes/inmuebleRoutes.ts
@@ -3,11 +3,12 @@ import upload from '../lib/multer';
 
 import { crearInmueble, getInmuebles, getInmuebleById } from '../controllers/inmuebleController';
 import checkIsAdmin from '../middlewares/checkIsAdmin';
+import checkAuth from '../middlewares/checkAuth';
 
 const router = Router();
 
 router.get('/', getInmuebles);
 router.get('/:id', getInmuebleById);
-router.post('/', checkIsAdmin, upload.array('imagenes', 3), crearInmueble);
+router.post('/', checkAuth, checkIsAdmin, upload.array('imagenes', 3), crearInmueble);
 
 export default router;
